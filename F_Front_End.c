03285eb2ad2ddbd584b500d69e1675e2c319203e
Nasim <mdnasimhosen333@gmail.com>
2025-04-23 21:17:23 +0600

    solved problem F

---
+++ F_Front_End.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int i = 0, j = n - 1;
+    while (i <= j)
+    {
+        if (i == j)
+        {
+            printf("%d", arr[i]);
+            break;
+        }
+        else
+        {
+            printf("%d %d ", arr[i], arr[j]);
+        }
+        i++;
+        j--;
+    }
+
+    return 0;
+}
